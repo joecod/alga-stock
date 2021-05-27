69f292f07e5c0ca36443bebd893355d75e595f1b
joecod <jose.lemes7@gmail.com>
2021-05-27 20:27:42 -0300

    reset css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      App
+      <h1>batata</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut pariatur aliquam doloremque error illo reiciendis! Perferendis earum reiciendis totam maiores.</p>
+      <ul>
+        <li>zé batata</li>
+        <li>cuzão </li>
+        <li>pau no cu</li>
+      </ul>
     </div>
   );
 }
