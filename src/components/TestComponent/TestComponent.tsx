8ad6b5fcd4ca9d3e38349eb030bbae878cfdbeed
joecod <jose.lemes7@gmail.com>
2021-05-27 19:11:07 -0300

    first useState

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TestComponent.css'
 
-const TestComponent = () => (
-    <div className="TestComponent">Test Component</div>
-)
+/* const TestComponent e function TestComponent são duas maneiras de implementar o componente funcional */
+// const TestComponent = () => (
+//     <div className="TestComponent">Test Component</div>
+// )
 
+function TestComponent (props: { name: string }) {
+    // const state = {
+    //     age: 21
+    // }
+
+    const[age, setAge] = useState(21)
+
+    return <div className="TestComponent">
+        {/* Olá, { props.name }, { state.age } */}
+        Olá, { props.name }, { age }
+        <button onClick= {() => {
+            // state.age++
+            // console.log(state.age)
+            setAge(age + 1)
+        }}>
+            +
+        </button>
+    </div>
+}
 export default TestComponent
